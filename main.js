bcb7221c4eff4f2c4600a6a42f868e2ce167d621
usamakhan2222 <161209226+usamakhan2222@users.noreply.github.com>
2024-03-17 16:22:37 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// bmi calculator in typescript
+let weight = 74;
+let hight = 6.2;
+let bmi = weight / (hight * hight);
+console.log(`usama your bmi is ${bmi}`);
+export {};
---
+++ main.ts
@@ -0,0 +1,7 @@
+// bmi calculator in typescript
+
+let weight =74;
+let hight = 6.2;
+
+let bmi = weight /( hight*hight)
+console.log(`usama your bmi is ${bmi}`)
